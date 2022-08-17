378c5f41d8637fad7e55de4df72a0710057bf54c
Harshana369 <harsha36998@gmail.com>
2022-08-17 08:54:23 +0530

    create subcribe part

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "./../components/Header";
-import ShopSection from "./../components/homeComponents/ShopSection"
+import ShopSection from "./../components/homeComponents/ShopSection";
+import CalltoActionSection from "./../components/homeComponents/CalltoActionSection";
+
 const HomeScreen = () => {
   return (
     <div>
       <Header />
       <ShopSection />
-        {/* <CalltoActionSection />
-        <ContactInfo />
+      <CalltoActionSection />
+      {/* {/* <ContactInfo />
         <Footer /> */}
     </div>
   );
