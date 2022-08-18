b7c5f29788e3450d248615212426aa7a51832a7a
Harshana369 <harsha36998@gmail.com>
2022-08-18 22:42:28 +0530

    class bug fix

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import "./responsive.css";
 import HomeScreen from "./screens/HomeScreen";
 import {BrowserRouter, Routes, Route } from "react-router-dom";
 import SingleProduct from "./screens/SingleProduct";
+import Login from "./screens/Login";
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomeScreen />}></Route>
-        {/* <Route path="/products/:id" component={SingleProduct} /> */}
         <Route path="/products/:id" element={<SingleProduct />}></Route>
-
-        {/* <Route path="products">
-        <Route path=":id" element={<SingleProduct />} />
-      </Route> */}
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   );
