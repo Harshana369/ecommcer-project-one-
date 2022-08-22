884dcf1bdc561f744cd9fc98c489aa03c5d25972
Harshana369 <harsha36998@gmail.com>
2022-08-22 16:54:40 +0530

    readux connect and product loard in backend

--- src/components/homeComponents/ShopSection.js
+++ src/components/homeComponents/ShopSection.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Pagination from "./pagination";
-import axios from "axios";
-
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../../Redux/Actions/PrductsAction";
+import Loading from "../LoadingError/Loading";
+import Message from "../LoadingError/Error";
 
 const ShopSection = () => {
+  const dispatch = useDispatch();
 
-  const [products, setProducts] = useState([]);
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
@@ -24,34 +24,46 @@ const ShopSection = () => {
           <div className="row">
             <div className="col-lg-12 col-md-12 article">
               <div className="shopcontainer row">
-                {products.map((product) => (
-                  <div
-                    className="shop col-lg-4 col-md-6 col-sm-6"
-                    key={product._id}
-                  >
-                    <div className="border-product">
-                      <Link to={`/products/${product._id}`}>
-                        <div className="shopBack">
-                          <img src={product.image} alt={product.name} />
-                        </div>
-                      </Link>
-
-                      <div className="shoptext">
-                        <p>
-                          <Link to={`/products/${product._id}`}>
-                            {product.name}
-                          </Link>
-                        </p>
-
-                        <Rating
-                          value={product.rating}
-                          text={`${product.numReviews} reviews`}
-                        />
-                        <h3>${product.price}</h3>
-                      </div>
-                    </div>
+                {loading ? (
+                  <div className="mb-3">
+                  
+                    <Loading />
                   </div>
-                ))}
+                ) : error ? (
+                  <Message variant={"alert-danger"}>{error}</Message>
+                ) : (
+                  <>
+                    {products.map((product) => (
+                      <div
+                        className="shop col-lg-4 col-md-6 col-sm-6"
+                        key={product._id}
+                      >
+                        <div className="border-product">
+                          <Link to={`/products/${product._id}`}>
+                            <div className="shopBack">
+                              <img src={product.image} alt={product.name} />
+                            </div>
+                          </Link>
+
+                          <div className="shoptext">
+                            <p>
+                              <Link to={`/products/${product._id}`}>
+                                {product.name}
+                              </Link>
+                            </p>
+
+                            <Rating
+                              value={product.rating}
+                              text={`${product.numReviews} reviews`}
+                            />
+                            <h3>${product.price}</h3>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </>
+                )}
+
                 {/* Pagination */}
                 <Pagination />
               </div>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createRoot } from "react-dom/client";
+import store from "./Redux/store";
+import { Provider } from "react-redux";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
